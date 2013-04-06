22c1cb8594521aa1ac0276c1007b17e542722ff1
inassjunus <inas.sofiyah@ui.ac.id>
2013-04-06 19:13:07 +0700

    Update SettingsScreen.java

--- cerita-nusantara/src/com/a4/ceritanusantara/views/SettingsScreen.java
+++ cerita-nusantara/src/com/a4/ceritanusantara/views/SettingsScreen.java
@@ -1,11 +1,14 @@
 package com.a4.ceritanusantara.views;
 
 import com.a4.ceritanusantara.Aplikasi;
-import com.a4.ceritanusantara.controllers.MainMenuController;
 import com.a4.ceritanusantara.controllers.SettingsController;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 
 
@@ -27,38 +30,46 @@ public class SettingsScreen extends AbstractScreen {
 	private boolean musicButtonPressed;
 	
 	private boolean debug = true;
+	private SpriteBatch spriteBatch;
+	private BitmapFont font;
 	
-	public SettingsScreen(Aplikasi app, int width, int height) {
-		super(app, width, height);
+	public SettingsScreen(Aplikasi app) {
+		super(app);
 		// TODO Auto-generated constructor stub
-		
+
 		background = new Texture(Gdx.files.internal("backgrounds/main_bg.png"));
 		
 		buttonBackground = new Texture(Gdx.files.internal("backgrounds/settings_bg.png"));
 		
-		soundOnTexture = 
+		//if(controller.getSettings().getBoolean("soundOn")) {
+			soundOnTexture = 
 				new Texture(Gdx.files.internal("buttons/toggle_sound_on.png"));
 		
-		soundOffTexture = 
-				new Texture(Gdx.files.internal("buttons/toggle_sound_off.png"));
-		
+		//} else {
+			soundOffTexture =
+					new Texture(Gdx.files.internal("buttons/toggle_sound_off.png"));
+		//}
+				
+		//if(controller.getSettings().getBoolean("musicOn")) {
 		musicOnTexture = 
 				new Texture(Gdx.files.internal("buttons/toggle_music_on.png"));
 		
-		musicOffTexture = 
-				new Texture(Gdx.files.internal("buttons/toggle_music_off.png"));
+		//} else {
+			musicOffTexture =
+					new Texture(Gdx.files.internal("buttons/toggle_music_off.png"));
+		//}
+				
 		
 		soundButtonBounds = new Rectangle((VIRTUAL_WIDTH-soundOnTexture.getWidth())/2,
-				440, soundOnTexture.getWidth(),
+				280, soundOnTexture.getWidth(),
 				soundOnTexture.getHeight()); 
 				
 		musicButtonBounds = new Rectangle((VIRTUAL_WIDTH-musicOnTexture.getWidth())/2,
-				440, musicOnTexture.getWidth(),
+				160, musicOnTexture.getWidth(),
 				musicOnTexture.getHeight());  
 		
 		soundButtonPressed = false;
 		musicButtonPressed = false;
-		
 		controller = new SettingsController(this);
 	}
 	
@@ -77,15 +88,42 @@ public class SettingsScreen extends AbstractScreen {
 			
 			batcher.draw(buttonBackground, (VIRTUAL_WIDTH-buttonBackground.getWidth())/2, 300);
 			
+			if(controller.getSettings().getBoolean("soundOn")) {
+				batcher.draw(soundOnTexture, 
+						(VIRTUAL_WIDTH-soundOnTexture.getWidth())/2+130, 380);
+				
+			} else {
+				batcher.draw(soundOffTexture, 
+						(VIRTUAL_WIDTH-soundOffTexture.getWidth())/2+130, 380);
+				
+			}
+			
+			if(controller.getSettings().getBoolean("musicOn")) {
+				batcher.draw(musicOnTexture, 
+						(VIRTUAL_WIDTH-musicOnTexture.getWidth())/2+130, 220);
+						
+			} else {
+				batcher.draw(musicOffTexture, 
+						(VIRTUAL_WIDTH-musicOffTexture.getWidth())/2+130, 220);
+					
+			}
+				
+				
 			//batcher.draw(soundOnTexture, 
-			//		(DEFAULT_WIDTH-soundOnTexture.getWidth())/2, 440);
+				//	(VIRTUAL_WIDTH-soundOnTexture.getWidth())/2, 440);
 			
 			//batcher.draw(musicOnTexture, 
-			//		(DEFAULT_WIDTH-musicOnTexture.getWidth())/2, 160);
+			//	(VIRTUAL_WIDTH-musicOnTexture.getWidth())/2, 160);
 			
 			
 			
 		batcher.end();
+		
+		if(debug){
+			drawDebug();
+		}
+		
+		controller.processInput();
 		
 		//--kalo mau ada sfx atau musik nanti di sini aja--
 		
@@ -103,5 +141,44 @@ public class SettingsScreen extends AbstractScreen {
 		return musicButtonBounds;
 	}
 	
+	public boolean musicButtonIsPressed() {
+		return musicButtonPressed;
+	}
+	
+	public boolean soundButtonIsPressed() {
+		return soundButtonPressed;
+	}
+	
+	public void setMusicButtonPressed(boolean b) {
+		this.musicButtonPressed = b;
+	}
+	
+	public void setSoundButtonPressed(boolean b) {
+		this.soundButtonPressed = b;
+	}
+	
+	//rectangle boundsnya digambar kalo mau debug==true
+		private void drawDebug(){
+			debugRenderer.setProjectionMatrix(cam.combined);
+			debugRenderer.begin(ShapeType.Rectangle);
+				if(soundButtonPressed){
+					debugRenderer.setColor(new Color(1, 1, 0, 1));
+				}
+				else{
+					debugRenderer.setColor(new Color(1, 0, 0, 1));
+				}
+				debugRenderer.rect(soundButtonBounds.x, soundButtonBounds.y,
+						soundButtonBounds.width, soundButtonBounds.height);
+				
+				if(musicButtonPressed){
+					debugRenderer.setColor(new Color(1, 1, 0, 1));
+				}
+				else{
+					debugRenderer.setColor(new Color(1, 0, 0, 1));
+				}
+				debugRenderer.rect(musicButtonBounds.x, musicButtonBounds.y,
+						musicButtonBounds.width, musicButtonBounds.height);
+			debugRenderer.end();
+		}
 
 }
