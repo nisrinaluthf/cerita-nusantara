9f8942f14475eec71e47afd98b2dcd77b227ee37
rizalbahr <rizal.bahriawan@ui.ac.id>
2013-04-12 15:30:23 +0700

    masukin semua background cerita

--- cerita-nusantara/src/com/a4/ceritanusantara/views/SubCeritaScreen.java
+++ cerita-nusantara/src/com/a4/ceritanusantara/views/SubCeritaScreen.java
@@ -1,9 +1,20 @@
 package com.a4.ceritanusantara.views;
 
 import com.a4.ceritanusantara.Aplikasi;
+import com.a4.ceritanusantara.controllers.PilihSubCeritaController;
+import com.a4.ceritanusantara.models.Cerita;
+import com.a4.ceritanusantara.models.SubCerita;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class SubCeritaScreen extends AbstractScreen{
 	public SubCeritaScreen(Aplikasi app){
 		super(app);
+		
 	}
 }
+
